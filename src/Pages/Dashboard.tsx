0e9231cb7cb7a0ee7545270cba90dc6476113a5e
Jamison T Blackwell <blackwellj1040@gmail.com>
2022-06-26 13:59:53 -0400

    frontend: added new components for dashboard + Profile page

---
+++ src/Pages/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Image, Box, Link } from '@chakra-ui/react'
+import DashInfo from "../components/DashInfo";
+
+const Dashboard = () => {
+
+  return (
+    <Container h='100%'>
+      <DashInfo />
+    </Container>
+  )
+}
+
+export default Dashboard;
---
+++ src/Pages/Profile.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => {
+
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+
+export default Profile;
